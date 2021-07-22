060ae639cab903b86c3754fe4f64ca57e0e9e721
Igor Matsak <matsak.i.v@gmail.com>
2021-07-22 19:12:19 +0300

    Fix tezos token contract type detection, fix canellation for request

--- Atomex.Client.Core/Blockchain/Tezos/BcdApi.cs
+++ Atomex.Client.Core/Blockchain/Tezos/BcdApi.cs
@@ -33,17 +33,28 @@ namespace Atomex.Blockchain.Tezos
         public List<string> Authors { get; set; }
         [JsonPropertyName("interfaces")]
         public List<string> Interfaces { get; set; }
+        [JsonPropertyName("contract_tags")]
+        public List<string> ContractTags { get; set; }
 
         public string GetContractType()
         {
+            if (ContractTags != null)
+            {
+                if (ContractTags.Contains("fa2"))
+                    return "FA2";
+
+                if (ContractTags.Contains("fa1-2"))
+                    return "FA12";
+            }
+
             if (Interfaces == null)
+                return "FA2";
+
+            if (Interfaces.FirstOrDefault(i => i == "TZIP-12" || i == "TZIP-012" || i.StartsWith("TZIP-012")) != null)
                 return "FA2";
 
             if (Interfaces.FirstOrDefault(i => i == "TZIP-7" || i == "TZIP-007" || i.StartsWith("TZIP-007")) != null)
                 return "FA12";
-
-            if (Interfaces.FirstOrDefault(i => i == "TZIP-12" || i == "TZIP-012" || i.StartsWith("TZIP-012")) != null)
-                return "FA2";
 
             return "FA2";
         }
