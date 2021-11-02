ac56688bed8278106ea239e08b0edf9ffaeb6781
Igor Matsak <matsak.i.v@gmail.com>
2021-11-02 14:00:53 +0300

    Add redeem for party deadline control for ERC20 swaps

--- Atomex.Client.Core/Swaps/Tezos/FA12/FA12Swap.cs
+++ Atomex.Client.Core/Swaps/Tezos/FA12/FA12Swap.cs
@@ -368,20 +368,20 @@ namespace Atomex.Swaps.Tezos.FA12
             Swap swap,
             CancellationToken cancellationToken = default)
         {
-            var fa12 = Fa12Config;
-
             if (swap.IsInitiator)
             {
                 var partyRedeemDeadline = swap.TimeStamp.ToUniversalTime().AddSeconds(DefaultAcceptorLockTimeInSeconds) - PartyRedeemTimeReserve;
 
                 if (DateTime.UtcNow > partyRedeemDeadline)
                 {
-                    Log.Error("Party redeem dedline reached for swap {@swap}", swap.Id);
+                    Log.Error("Party redeem deadline reached for swap {@swap}", swap.Id);
                     return;
                 }
             }
 
             Log.Debug("Create redeem for acceptor for swap {@swapId}", swap.Id);
+
+            var fa12 = Fa12Config;
 
             var walletAddress = (await Fa12Account
                 .GetUnspentAddressesAsync(
