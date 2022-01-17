98975beb5e490ddb1257ada766f94dcf148a1333
Igor Matsak <matsak.i.v@gmail.com>
2022-01-17 18:33:33 +0300

    Move WalletAddressViewModel to core library

---
+++ Atomex.Client.Core/ViewModels/AddressesHelper.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Atomex.Core;
+using Atomex.Wallet.Abstract;
+
+namespace Atomex.ViewModels
+{
+    public static class AddressesHelper
+    {
+        public static async Task<IEnumerable<WalletAddressViewModel>> GetReceivingAddressesAsync(
+            IAccount account,
+            CurrencyConfig currency,
+            string tokenContract = null)
+        {
+            // get all addresses with tokens (if exists)
+            var tokenAddresses = Currencies.HasTokens(currency.Name)
+                ? (account.GetCurrencyAccount(currency.Name) is IHasTokens accountWithTokens
+                    ? await accountWithTokens
+                        .GetUnspentTokenAddressesAsync()
+                        .ConfigureAwait(false) ?? new List<WalletAddress>()
+                    : new List<WalletAddress>())
+                : new List<WalletAddress>();
+
+            // get all active addresses
+            var activeAddresses = (await account
+                .GetUnspentAddressesAsync(currency.Name)
+                .ConfigureAwait(false))
+                .ToList();
+
+            // get free external address
+            var freeAddress = await account
+                .GetFreeExternalAddressAsync(currency.Name)
+                .ConfigureAwait(false);
+
+            return activeAddresses
+                .Concat(tokenAddresses)
+                .Concat(new WalletAddress[] { freeAddress })
+                .GroupBy(w => w.Address)
+                .Select(g =>
+                {
+                    // main address
+                    var address = g.FirstOrDefault(w => w.Currency == currency.Name);
+
+                    var isFreeAddress = address?.Address == freeAddress.Address;
+
+                    var hasTokens = g.Any(w => w.Currency != currency.Name);
+
+                    var tokenAddresses = tokenContract != null
+                        ? g.Where(w => w.TokenBalance?.Contract == tokenContract)
+                        : Enumerable.Empty<WalletAddress>();
+
+                    var hasSeveralTokens = tokenAddresses.Count() > 1;
+
+                    var tokenAddress = tokenAddresses.FirstOrDefault();
+
+                    var tokenBalance = hasSeveralTokens
+                        ? tokenAddresses.Count()
+                        : tokenAddress?.Balance ?? 0m;
+
+                    var showTokenBalance = hasSeveralTokens
+                        ? tokenBalance != 0
+                        : tokenContract != null && tokenAddress?.TokenBalance?.Symbol != null;
+
+                    var tokenCode = hasSeveralTokens
+                        ? "TOKENS"
+                        : tokenAddress?.TokenBalance?.Symbol ?? "";
+
+                    return new WalletAddressViewModel
+                    {
+                        Address          = g.Key,
+                        HasActivity      = address?.HasActivity ?? hasTokens,
+                        AvailableBalance = address?.AvailableBalance() ?? 0m,
+                        CurrencyFormat   = currency.Format,
+                        CurrencyCode     = currency.Name,
+                        IsFreeAddress    = isFreeAddress,
+                        ShowTokenBalance = showTokenBalance,
+                        TokenBalance     = tokenBalance,
+                        TokenFormat      = "F8",
+                        TokenCode        = tokenCode
+                    };
+                });
+        }
+    }
+}
---
+++ Atomex.Client.Core/ViewModels/WalletAddressViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Atomex.ViewModels
+{
+    public class WalletAddressViewModel
+    {
+        public string Address { get; set; }
+        public bool HasActivity { get; set; }
+        public decimal AvailableBalance { get; set; }
+        public string CurrencyFormat { get; set; }
+        public string CurrencyCode { get; set; }
+        public bool IsFreeAddress { get; set; }
+        public bool ShowTokenBalance { get; set; }
+        public decimal TokenBalance { get; set; }
+        public string TokenFormat { get; set; }
+        public string TokenCode { get; set; }
+    }
+}
