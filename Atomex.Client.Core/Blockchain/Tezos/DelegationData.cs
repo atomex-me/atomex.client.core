c58bd60565675d496f1025d4dea77cf7086dddc0
Konstantin <johnysalvesen@gmail.com>
2022-04-11 17:02:08 +0300

    Added DelegationStatus and Delegation structs;

---
+++ Atomex.Client.Core/Blockchain/Tezos/DelegationData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Atomex.Blockchain.Tezos
+{
+    public enum DelegationStatus
+    {
+        Pending,
+        Confirmed,
+        Active
+    }
+    
+    public class BakerData
+    {
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public decimal Fee { get; set; }
+        public decimal MinDelegation { get; set; }
+        public decimal StakingAvailable { get; set; }
+        public decimal EstimatedRoi { get; set; }
+    }
+
+    public class Delegation
+    {
+        public BakerData Baker { get; set; }
+        public string Address { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime DelegationTime { get; set; }
+        public DelegationStatus Status { get; set; }
+    }
+}
