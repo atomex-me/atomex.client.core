6f3a017f13488adb2e4db35ce38845a5e5160a29
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-04-14 16:36:11 +0300

    Added basic interface of TzktEventsClient.

---
+++ Atomex.Client.Core/Services/Abstract/ITzktEventsClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Atomex.Blockchain.Tezos;
+
+
+namespace Atomex.Services.Abstract
+{
+    public interface ITzktEventsClient
+    {
+        string BaseUrl { get; }
+
+        Task Start();
+        Task Stop();
+    }
+}
