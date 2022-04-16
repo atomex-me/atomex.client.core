6fce5bdcb8b2da98a09a1ea50aa16ce9081838f5
Igor Matsak <matsak.i.v@gmail.com>
2022-04-16 11:36:48 +0300

    Remove unused files, fix github actions

--- Atomex.Client.Core/Cryptography/Algorithms.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Atomex.Cryptography
-{
-    public class Algorithms
-    {
-        public const string Ec = "EC";
-        public const string Ecdsa = "ECDSA";
-        public const string Sha256WithEcdsa = "SHA256withECDSA";
-    }
-}
--- Atomex.Client.Core/Cryptography/ISigner.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Atomex.Cryptography
-{
-    public interface ISigner
-    {
-        bool VerifySign(byte[] data, byte[] sign, byte[] publicKey);
-        byte[] Sign(byte[] data, byte[] privateKey);
-    }
-}
