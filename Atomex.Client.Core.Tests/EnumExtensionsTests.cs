25b4ddb06482679f1e616021c9b8dda32dd97bda
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-04-18 17:19:54 +0300

    Added EnumExtensions with GetDescription method and unit tests for it.

---
+++ Atomex.Client.Core.Tests/EnumExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atomex.Common;
+using Xunit;
+
+namespace Atomex.Client.Core.Tests
+{
+    public enum TestEnum
+    {
+        [Description("transaction")]
+        Transaction,
+
+        [Description("set_deposits_limit")]
+        SetDepositsLimit,
+
+        Origination,
+    }
+
+    public struct TestNotEnum
+    {
+
+    }
+
+
+    public class EnumExtensionsTests
+    {
+        [Fact]
+        public void GetDescription_OnNotEnum_ThrowsException()
+        {
+            var notEnum = new TestNotEnum();
+
+            Assert.Throws<ArgumentException>(() => notEnum.GetDescription());
+        }
+
+        [Fact]
+        public void GetDescription_OnEnumWithDescription_ReturnsDescription()
+        {
+            var transactionDescription = TestEnum.Transaction.GetDescription();
+            var setDepositsLimitsDescription = TestEnum.SetDepositsLimit.GetDescription();
+
+            Assert.Equal("transaction", transactionDescription);
+            Assert.Equal("set_deposits_limit", setDepositsLimitsDescription);
+        }
+
+        [Fact]
+        public void GetDescription_OnEnumWithoutDescription_ReturnsToString()
+        {
+            var originationDescription = TestEnum.Origination.GetDescription();
+
+            Assert.Equal("Origination", originationDescription);
+        }
+    }
+}
---
+++ Atomex.Client.Core/Common/EnumExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Atomex.Common
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription<T>(this T enumerationValue)
+            where T : struct
+        {
+            var type = enumerationValue.GetType();
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("EnumerationValue must be of Enum type", nameof(enumerationValue));
+            }
+            
+            var memberInfo = type.GetMember(enumerationValue.ToString());
+            if (memberInfo.Length == 0)
+            {
+                return enumerationValue.ToString();
+            }
+
+            var attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attrs.Length > 0
+                ? ((DescriptionAttribute)attrs[0]).Description
+                : enumerationValue.ToString();
+        }
+	}
+}
