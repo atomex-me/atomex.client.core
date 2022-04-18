af30a4d2a5a3527466cf7713de6330953d5d00eb
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-04-18 12:45:18 +0300

    Added TzktEvents project.

---
+++ Atomex.TzktEvents/ITzktEventsClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Atomex.TzktEvents
+{
+    public interface ITzktEventsClient
+    {
+        string BaseUri { get; }
+
+        Task Start();
+        Task Stop();
+    }
+}
---
+++ Atomex.TzktEvents/TzktEventsClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+
+namespace Atomex.TzktEvents
+{
+    public class TzktEventsClient : ITzktEventsClient
+    {
+        public string BaseUri { get; }
+        private string EventsUri => $"https://{BaseUri}/v1/events";
+
+        private readonly HubConnection _connection;
+
+
+        public TzktEventsClient(string baseUri)
+        {
+            BaseUri = baseUri;
+
+            _connection = new HubConnectionBuilder()
+                .WithUrl(EventsUri)
+                .Build();
+
+            _connection.Closed += Init;
+        }
+
+        private async Task Init(Exception? arg = null)
+        {
+            await _connection.StartAsync();
+            // TODO: Add subscribers on channels.
+        }
+
+        public async Task Start()
+        {
+            await Init();
+        }
+
+        public async Task Stop()
+        {
+            _connection.Closed -= Init;
+            await _connection.StopAsync();
+            await _connection.DisposeAsync();
+        }
+    }
+}
