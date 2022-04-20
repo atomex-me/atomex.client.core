386ae3ac6cc1f8faf4b4d7dd61dd4bcc0e159e5f
Konstantin <johnysalvesen@gmail.com>
2022-04-20 18:45:02 +0300

    Added Counter and Nonce in TezosTokenTransfer ID; (#91)

---
+++ Atomex.Client.Core/Blockchain/Tezos/Tzkt/TokenOperation.cs
@@ -0,0 +1,11 @@
+using System.Text.Json.Serialization;
+
+namespace Atomex.Blockchain.Tezos.Tzkt
+{
+    public class TokenOperation
+    {
+        [JsonPropertyName("hash")] public string Hash { get; set; }
+        [JsonPropertyName("counter")] public int Counter { get; set; }
+        [JsonPropertyName("nonce")] public int? Nonce { get; set; }
+    }
+}
