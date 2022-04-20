52a7ebc7305c5407d2b8074aaaf73052cad7749d
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-04-20 13:53:36 +0300

    Added IAccountService interface. #86

---
+++ Atomex.TzktEvents/Services/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace Atomex.TzktEvents.Services
+{
+    public interface IAccountService : IDisposable
+    {
+        Task NotifyOnAccountAsync(string address, Action handler);
+        Task InitAsync();
+        void SetSubscriptions();
+    }
+}
