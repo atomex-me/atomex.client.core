18a21bbcde7f7342f2b45a07e7f6788c8093f021
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-05-16 16:53:41 +0300

    Added ISoChainRealtimeApi interface.

---
+++ Atomex.Client.Core/Blockchain/SoChain/ISoChainRealtimeApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Atomex.Blockchain.SoChain
+{
+    public interface ISoChainRealtimeApi
+    {
+        string HostUrl { get; }
+
+        event EventHandler Connected;
+        event EventHandler Reconnecting;
+        event EventHandler Disconnected;
+
+        Task StartAsync();
+        Task StopAsync();
+
+        Task SubscribeOnBalanceUpdateAsync(string network, string address, Action<string> handler);
+        Task SubscribeOnBalanceUpdateAsync(string network, IEnumerable<string> addresses, Action<string> handler);
+
+        Task UnsubscribeOnBalanceUpdateAsync(string network, string address, Action<string> handler = null);
+        Task UnsubscribeOnBalanceUpdateAsync(string network, IEnumerable<string> addresses, Action<string> handler = null);
+    }
+}
