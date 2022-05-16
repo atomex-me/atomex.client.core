9aa570788cc2f877cc1e248c52559877a28f6fdc
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-05-16 17:02:04 +0300

    Added basic frame of SoChainRealtimeApi.

---
+++ Atomex.Client.Core/Blockchain/SoChain/SoChainRealtimeApi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PusherClient;
+
+
+namespace Atomex.Blockchain.SoChain
+{
+    public class SoChainRealtimeApi : ISoChainRealtimeApi
+    {
+        public string HostUrl { get; }
+
+        public event EventHandler Connected;
+        public event EventHandler Reconnecting;
+        public event EventHandler Disconnected;
+
+        private bool _isStarted;
+        private Pusher _pusher;
+        private readonly ConcurrentDictionary<string, Channel> _channels = new();
+
+
+        public SoChainRealtimeApi(string hostUrl)
+        {
+            HostUrl = hostUrl;
+        }
+
+        public Task StartAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SubscribeOnBalanceUpdateAsync(string network, string address, Action<string> handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SubscribeOnBalanceUpdateAsync(string network, IEnumerable<string> addresses, Action<string> handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UnsubscribeOnBalanceUpdateAsync(string network, string address, Action<string> handler = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UnsubscribeOnBalanceUpdateAsync(string network, IEnumerable<string> addresses, Action<string> handler = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
