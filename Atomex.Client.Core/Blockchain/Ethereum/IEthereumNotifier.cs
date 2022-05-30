2e6df1818b06f31b77386970ba7725715add2572
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-05-30 15:53:50 +0300

    Added interface for EthereumNotifier service.

---
+++ Atomex.Client.Core/Blockchain/Ethereum/IEthereumNotifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace Atomex.Blockchain.Ethereum
+{
+    public interface IEthereumNotifier
+    {
+        string BaseUrl { get; }
+
+        Task StartAsync();
+        Task StopAsync();
+
+
+        Task SubscribeOnBalanceUpdate(string address, Action<string> handler);
+        Task SubscribeOnBalanceUpdate(IEnumerable<string> addresses, Action<string> handler);
+    }
+}
