c8b6eaacce5cfb070fe1e4e4b4154637f9f68006
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-05-31 22:27:52 +0300

    Implemented RunBalanceChecker runner for ether.

--- Atomex.Client.Core/Blockchain/Ethereum/EthereumNotifier.cs
+++ Atomex.Client.Core/Blockchain/Ethereum/EthereumNotifier.cs
@@ -2,8 +2,16 @@
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using Atomex.Common;
+using Atomex.Core;
 using Atomex.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Serilog;
+using static System.String;
 
 
 namespace Atomex.Blockchain.Ethereum
@@ -15,9 +23,16 @@ namespace Atomex.Blockchain.Ethereum
         private readonly ILogger _log;
 
         private WebSocketClient _events;
-        private bool _isConnected;
+        private bool _isRunning;
 
         private readonly ConcurrentDictionary<string, Action<string>> _actions = new();
+
+        private readonly TimeSpan _transactionsDelay = TimeSpan.FromSeconds(15);
+        private const int MinDelayBetweenRequestMs = 1000;
+        private static readonly RequestLimitControl RequestLimitControl 
+            = new(MinDelayBetweenRequestMs);
+        private CancellationTokenSource _cts;
+        private int _startBlock = 6980640; // TODO: Get current last block as initial value
 
         public EthereumNotifier(string baseUrl, string eventsWs, ILogger log)
         {
@@ -28,17 +43,20 @@ namespace Atomex.Blockchain.Ethereum
 
         public async Task StartAsync()
         {
-            if (_isConnected)
+            if (_isRunning)
             {
                 return;
             }
 
             try
             {
-                _isConnected = true;
+                _isRunning = true;
+                _cts = new CancellationTokenSource();
 
                 _events = new WebSocketClient(_eventsWs);
                 await _events.ConnectAsync();
+
+                RunBalanceChecker();
             }
             catch (Exception e)
             {
@@ -48,13 +66,15 @@ namespace Atomex.Blockchain.Ethereum
 
         public async Task StopAsync()
         {
-            if (!_isConnected)
+            if (!_isRunning)
             {
                 return;
             }
 
             try
             {
+                _cts.Cancel();
+
                 await _events.CloseAsync();
                 _actions.Clear();
             }
@@ -65,7 +85,7 @@ namespace Atomex.Blockchain.Ethereum
             }
             finally
             {
-                _isConnected = false;
+                _isRunning = false;
             }
         }
 
@@ -83,6 +103,63 @@ namespace Atomex.Blockchain.Ethereum
             }
 
             return Task.CompletedTask;
+        }
+
+        private void RunBalanceChecker()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    foreach (var (address, handler) in _actions)
+                    {
+                        await RequestLimitControl
+                            .Wait(_cts.Token)
+                            .ConfigureAwait(false);
+                        
+                        var requestUri = $"api?module=account" +
+                                         "&action=txlist" +
+                                         $"&address={address}" +
+                                         "&tag=latest" +
+                                         "&page=1" +
+                                         $"&startBlock={_startBlock}";
+                        
+                        var resultLength = await HttpHelper.GetAsyncResult<int>(
+                                baseUri: BaseUrl,
+                                requestUri: requestUri,
+                                responseHandler: (response, content) =>
+                                {
+                                    var json = JsonConvert.DeserializeObject<JObject>(content);
+
+                                    if (json.ContainsKey("status") && json["status"]!.ToString() != "1")
+                                    {
+                                        _log.Warning("Status is NOTOK from Etherscan, response: {@Response}", json);
+                                        return 0;
+                                    }
+
+                                    return json.ContainsKey("result") ? json["result"]!.Count() : 0;
+                                },
+                                cancellationToken: _cts.Token)
+                            .ConfigureAwait(false);
+
+                        _log.Information("Got resultLength = {ResultLength}", resultLength);
+                        if (resultLength.Value > 0)
+                        {
+                            try
+                            {
+                                handler(address);
+                            }
+                            catch (Exception e)
+                            {
+                                _log.Error(e, "Caught error on ether balance updated handler call");
+                            }
+                        }
+                    }
+
+                    await Task.Delay(_transactionsDelay);
+                    _startBlock++;
+                }
+            });
         }
     }
 }
