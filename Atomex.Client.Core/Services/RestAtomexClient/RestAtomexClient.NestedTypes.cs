2fa0451df2ed376aff4d5f192e033084fe52a95a
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2022-06-04 02:40:39 +0300

    Extract the nested types of RestAtomexClient into a separate file
    Make the RestAtomexClient as partial

---
+++ Atomex.Client.Core/Services/RestAtomexClient/RestAtomexClient.NestedTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atomex.Services;
+
+public partial class RestAtomexClient
+{
+    private record AuthenticationRequestContent(
+        string Message,
+        long TimeStamp,
+        string PublicKey,
+        string Signature,
+        string Algorithm
+    );
+
+    private record AuthenticationData(string Id, string Token, long Expires);
+}
