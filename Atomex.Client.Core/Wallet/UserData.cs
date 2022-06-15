ee22329a37db140c705f9ee4dd5a3db7f3b5337e
Konstantin <johnysalvesen@gmail.com>
2022-06-15 18:59:43 +0300

    Added HideTokensWithLowBalance to UserData;

--- Atomex.Client.Core/Wallet/UserData.cs
+++ Atomex.Client.Core/Wallet/UserData.cs
@@ -40,6 +40,7 @@ namespace Atomex.Wallet
         public int BalanceUpdateIntervalInSec { get; set; }
         public string[] InitializedCurrencies { get; set; }
         public string[] DisabledTokens { get; set; }
+        public bool? HideTokensWithLowBalance { get; set; }
         public List<AtomexNotification> Notifications { get; set; }
 
         public static UserData GetDefaultSettings(ICurrencies currencies)
