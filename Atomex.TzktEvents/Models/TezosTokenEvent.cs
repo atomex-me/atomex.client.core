fd6b6e55e8a50b7be44556d57a8b5bb6f1e7c7b6
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-06-21 12:58:21 +0300

    Added TezosTokenEvent model.

---
+++ Atomex.TzktEvents/Models/TezosTokenEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace Atomex.TzktEvents.Models
+{
+    public class TezosTokenEvent
+    {
+        public string Standard = string.Empty;
+        public string Contract = string.Empty;
+        public decimal TokenId = 0;
+        public string Token = string.Empty;
+        public string Address;
+
+        public TezosTokenEvent(string address)
+        {
+            Address = address;
+        }
+
+        public TezosTokenEvent(string standard, string contract, decimal tokenId, string token, string address) : this(address)
+        {
+            Standard = standard;
+            Contract = contract;
+            TokenId = tokenId;
+            Token = token;
+        }
+    }
+}
