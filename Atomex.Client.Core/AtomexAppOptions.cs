74383ed14c857bf3295e0d017e134c6cdfd7e728
Igor Matsak <matsak.i.v@gmail.com>
2022-07-17 14:29:23 +0300

    Remove file-scoped namespace declaration for backward compatibility

--- Atomex.Client.Core/AtomexAppOptions.cs
+++ Atomex.Client.Core/AtomexAppOptions.cs
@@ -2,30 +2,32 @@
 
 using Atomex.Swaps;
 
-namespace Atomex;
-
-public record AtomexAppOptions
+namespace Atomex
 {
-    public SwapManagerOptions SwapManager { get; init; }
 
-    public static AtomexAppOptions Default => new()
+    public record AtomexAppOptions
     {
-        SwapManager = SwapManagerOptions.Default
-    };
+        public SwapManagerOptions SwapManager { get; init; }
 
-    public static AtomexAppOptions LoadFromConfiguration(IConfiguration configuration)
-    {
-        const string USE_WATCH_TOWER_MOVE_PATH = "SwapManager:UseWatchTowerMode";
-
-        if (!bool.TryParse(configuration[USE_WATCH_TOWER_MOVE_PATH], out var useWatchTowerMode))
-            return Default;
-        
-        return Default with
+        public static AtomexAppOptions Default => new()
         {
-            SwapManager = SwapManagerOptions.Default with
-            {
-                UseWatchTowerMode = useWatchTowerMode
-            }
+            SwapManager = SwapManagerOptions.Default
         };
+
+        public static AtomexAppOptions LoadFromConfiguration(IConfiguration configuration)
+        {
+            const string USE_WATCH_TOWER_MOVE_PATH = "SwapManager:UseWatchTowerMode";
+
+            if (!bool.TryParse(configuration[USE_WATCH_TOWER_MOVE_PATH], out var useWatchTowerMode))
+                return Default;
+
+            return Default with
+            {
+                SwapManager = SwapManagerOptions.Default with
+                {
+                    UseWatchTowerMode = useWatchTowerMode
+                }
+            };
+        }
     }
 }
